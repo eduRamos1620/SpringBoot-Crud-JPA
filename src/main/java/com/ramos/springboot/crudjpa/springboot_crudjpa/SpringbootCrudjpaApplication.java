b35d986c2deab9c276f7fca9054b573db2b45d34
eduRamos1620 <eduardoramosrosales16@gmail.com>
2025-04-01 18:08:04 -0600

    Se crea el proyecto crud con jpa

---
+++ src/main/java/com/ramos/springboot/crudjpa/springboot_crudjpa/SpringbootCrudjpaApplication.java
@@ -0,0 +1,13 @@
+package com.ramos.springboot.crudjpa.springboot_crudjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCrudjpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCrudjpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ramos/springboot/crudjpa/springboot_crudjpa/SpringbootCrudjpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramos.springboot.crudjpa.springboot_crudjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCrudjpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

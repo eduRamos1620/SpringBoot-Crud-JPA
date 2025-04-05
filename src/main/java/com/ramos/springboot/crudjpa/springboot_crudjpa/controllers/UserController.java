a7a5284ed73ac3637163b55b0002839e5f5268ba
eduRamos1620 <eduardoramosrosales16@gmail.com>
2025-04-04 18:38:14 -0600

    Se crean metodos para encriptar password y para crear y asignar usurios con su tipo de role

---
+++ src/main/java/com/ramos/springboot/crudjpa/springboot_crudjpa/controllers/UserController.java
@@ -0,0 +1,54 @@
+package com.ramos.springboot.crudjpa.springboot_crudjpa.controllers;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ramos.springboot.crudjpa.springboot_crudjpa.entities.User;
+import com.ramos.springboot.crudjpa.springboot_crudjpa.services.UserService;
+
+import jakarta.validation.Valid;
+
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    
+    @Autowired
+    private UserService service;
+
+    @GetMapping
+    public List<User> list(){
+        return service.findAll();
+    }
+
+    @PostMapping
+    public ResponseEntity<?> create(@Valid @RequestBody User user, BindingResult result){
+        if(result.hasFieldErrors()){
+            return validation(result);
+        }
+       
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.save(user));
+    }
+
+    private ResponseEntity<?> validation(BindingResult result) {
+        Map<String, String> errors = new HashMap<>();
+        
+        result.getFieldErrors().forEach(error -> {
+            errors.put(error.getField(), "El campo " + error.getField() + " " + error.getDefaultMessage());
+        });
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+}
--- src/main/java/com/ramos/springboot/crudjpa/springboot_crudjpa/services/UserServiceImpl.java
+++ src/main/java/com/ramos/springboot/crudjpa/springboot_crudjpa/services/UserServiceImpl.java
@@ -1,12 +1,15 @@
 package com.ramos.springboot.crudjpa.springboot_crudjpa.services;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.ramos.springboot.crudjpa.springboot_crudjpa.entities.Role;
 import com.ramos.springboot.crudjpa.springboot_crudjpa.entities.User;
 import com.ramos.springboot.crudjpa.springboot_crudjpa.repositories.RoleRepository;
 import com.ramos.springboot.crudjpa.springboot_crudjpa.repositories.UserRepository;
@@ -32,6 +35,19 @@ public class UserServiceImpl implements UserService{
     @Override
     @Transactional
     public User save(User user) {
+
+        Optional<Role> optionalRoleUser = roleRepository.findByName("ROLE_USER");
+        List<Role> roles =  new ArrayList<>();
+        
+        optionalRoleUser.ifPresent(role -> roles.add(role));
+
+        if(user.isAdmin()){
+            Optional<Role> optionalRoleAdmin = roleRepository.findByName("ROLE_ADMIN");
+            optionalRoleAdmin.ifPresent(role -> roles.add(role));
+        }
+
+        user.setRoles(roles);
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
     

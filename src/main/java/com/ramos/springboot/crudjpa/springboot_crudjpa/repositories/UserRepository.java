54d8f1fba583bca7e0007a2b62f12b450e25b2de
eduRamos1620 <eduardoramosrosales16@gmail.com>
2025-04-05 19:20:45 -0600

    Se implementa el componente UserDetailsService

--- src/main/java/com/ramos/springboot/crudjpa/springboot_crudjpa/repositories/UserRepository.java
+++ src/main/java/com/ramos/springboot/crudjpa/springboot_crudjpa/repositories/UserRepository.java
@@ -1,5 +1,7 @@
 package com.ramos.springboot.crudjpa.springboot_crudjpa.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.ramos.springboot.crudjpa.springboot_crudjpa.entities.User;
@@ -7,4 +9,6 @@ import com.ramos.springboot.crudjpa.springboot_crudjpa.entities.User;
 public interface UserRepository extends CrudRepository<User, Long>{
     
     boolean existsByUsername(String username);
+
+    Optional<User> findByUsername(String username);
 }

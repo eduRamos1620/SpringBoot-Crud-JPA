becb432a74498304e0a8bdd3012d451778f9fd82
eduRamos1620 <eduardoramosrosales16@gmail.com>
2025-04-10 20:59:04 -0600

    Se agrego el metodo para valdiar el login y se creo el JWT

--- src/main/java/com/ramos/springboot/crudjpa/springboot_crudjpa/services/JpaUserDetailsService.java
+++ src/main/java/com/ramos/springboot/crudjpa/springboot_crudjpa/services/JpaUserDetailsService.java
@@ -10,11 +10,13 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.ramos.springboot.crudjpa.springboot_crudjpa.entities.User;
 import com.ramos.springboot.crudjpa.springboot_crudjpa.repositories.UserRepository;
 
+@Service
 public class JpaUserDetailsService implements UserDetailsService{
 
     @Autowired
